62170054bd977c9d06f7c719c515ae7cade5b58f
Charles Vater <vater7280@uwlax.edu>
2026-09-24 10:13:28 -0500

    initial commit

---
+++ src/test/java/uwlax/cs440/appointmentBooking/AppointmentBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package uwlax.cs440.appointmentBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
